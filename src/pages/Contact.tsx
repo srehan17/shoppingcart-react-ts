c85bf13ad160ab736421cbfe849bed97df3e4eee
Sarah Rehan <sarahahmad17@gmail.com>
2026-04-21 04:53:49 +0000

    updated text

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
       <h2 className="text-center mb-4">Contact Us</h2>
       <div className="text-center" style={{marginTop: 100}}>
         <div className="h4" style={{marginBottom: 40}}>Feedback? Questions? </div>
-        <p>Reach us by email at feedback@mystore.com</p>
+        <p>Reach us by email at contact@mystore.com</p>
       </div>
     </>
   )
