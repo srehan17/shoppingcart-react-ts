d2db6e17019b6c3505d48db58e4aa7bbaa423036
Sarah Rehan <sarahahmad17@gmail.com>
2026-04-26 11:53:52 -0400

    add Spanish locale

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,11 +2,13 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en/translation.json";
 import fr from "./locales/fr/translation.json";
+import es from "./locales/es/translation.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     fr: { translation: fr },
+    es: { translation: es },
   },
   lng: "en",
   fallbackLng: "en",
